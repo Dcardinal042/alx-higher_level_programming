e7093a34713c0a10557b60d43562b9a573b0c3fd
Dcardinal042 <Samokoh40@gmail.com>
2023-05-09 11:10:49 -0700

    3-print_alphabt.py

--- 3-print_alphabt.py
+++ 3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i in [101, 113]:
+for letter in range(97, 123):
+    if letter == 101 or letter == 113:
         continue
-    print('{:c}'.format(i), end="")
+    else:
+        print("{}".format(chr(letter)), end="")
