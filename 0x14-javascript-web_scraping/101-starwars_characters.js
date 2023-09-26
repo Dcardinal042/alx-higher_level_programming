7c0471cfe9ed16176aa4794f1a7ffc9088cc5283
Dcardinal042 <samokoh40@gmail.com>
2023-09-26 05:03:13 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,46 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
+// Function to fetch characters from a specific movie
+function getMovieCharacters(movieId) {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+    } else {
+      const filmData = JSON.parse(body);
+      const characters = filmData.characters;
+
+      // Function to fetch and print character names
+      function fetchCharacterName(index) {
+        if (index >= characters.length) {
+          return; // All characters have been printed
+        }
+
+        const characterUrl = characters[index];
+        request(characterUrl, (error, response, body) => {
+          if (error) {
+            console.error('Error:', error);
+          } else {
+            const characterData = JSON.parse(body);
+            console.log(characterData.name);
+            fetchCharacterName(index + 1); // Print the next character
+          }
+        });
       }
+
+      // Start fetching and printing character names
+      fetchCharacterName(0);
     }
   });
+}
+
+// Check if the movie ID is provided as a command-line argument
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Please provide a movie ID as the first argument.');
+} else {
+  getMovieCharacters(movieId);
 }
