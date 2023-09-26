bcbef0987c8e479dc0db0b336093bb4da66c7bc9
Dcardinal042 <samokoh40@gmail.com>
2023-09-26 05:15:19 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,47 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const characters = resp.characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.log('Please provide a valid Movie ID as the first argument.');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+function getCharacters(url) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      process.exit(1);
     }
-  }
-});
+
+    if (response.statusCode !== 200) {
+      console.error('Invalid response:', response.statusCode);
+      process.exit(1);
+    }
+
+    const film = JSON.parse(body);
+
+    console.log(`Characters from "${film.title}":`);
+    film.characters.forEach(characterUrl => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.error('Error:', charError);
+          process.exit(1);
+        }
+
+        if (charResponse.statusCode !== 200) {
+          console.error('Invalid response:', charResponse.statusCode);
+          process.exit(1);
+        }
+
+        const character = JSON.parse(charBody);
+        console.log(character.name);
+      });
+    });
+  });
+}
+
+getCharacters(apiUrl);
