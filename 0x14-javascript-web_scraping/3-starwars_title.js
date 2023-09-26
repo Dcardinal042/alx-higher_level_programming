ef2fed59a308308ef3da4661a6d3dd067f49ce61
Dcardinal042 <samokoh40@gmail.com>
2023-09-26 05:22:22 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,31 @@
 #!/usr/bin/node
 const request = require('request');
-const id = process.argv[2];
-request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
-  if (error == null) {
-    const json = JSON.parse(body);
-    console.log(json.title);
+
+// Check if a movie ID argument is provided
+if (process.argv.length !== 3) {
+  console.error('Usage: node 3-starwars_title.js <movie_id>');
+  process.exit(1);
+}
+
+// Get the movie ID from the command line argument
+const movieId = process.argv[2];
+
+// Construct the URL for the SWAPI API endpoint
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Send a GET request to the SWAPI API
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error(`HTTP Error: ${response.statusCode}`);
+  } else {
+    try {
+      const movie = JSON.parse(body);
+      console.log(movie.title);
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+    }
   }
 });
+
